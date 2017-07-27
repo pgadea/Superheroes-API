1ddaaa7822bf9ac0b387b2f9e24321eceda8e6d9
Pierre <pgadea7@gmail.com>
2017-07-26 22:07:55 -0500

    added PUT and DELETE

--- server.js
+++ server.js
@@ -58,7 +58,38 @@ app.post('/api', (req, res) => {
   })
 })
 
+// PUT SINGLE HERO
+app.put('/api/:hero_id', (req,res) => {
 
+  // FIND our hero to update
+  Superhero.findById(req.params.hero_id, (err, superhero) => {
+
+    // If new data then change value, else keep the same
+    superhero.name = (req.body.name) ? req.body.name : superhero.name
+    superhero.superpower = (req.body.superhero) ? req.body.superhero : superhero.superpower
+    superhero.image = (req.body.image) ? req.body.image : superhero.image
+
+    superhero.save((err, superhero) => {
+      if(err){
+        res.json({message: `Successfully updated hero: ${superhero.name}`, data: superhero})
+      }
+    })
+  })
+})
+
+// DELETE SINGLE RECORD
+app.delete('/api/:hero_id', (req,res) => {
+
+  // Find hero to delete via search object
+  Superhero.remove({_id: req.params.hero_id}, (err) => {
+    if(err){
+      res.send({message: err, data: null})
+    }
+    else{
+      res.send({message: `Superhero successfully deleted!`, data: {}})
+    }
+  })
+})
 
 // SERVER
 app.listen(port, () => console.log(`Listen on port: ${port}`))
