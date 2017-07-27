e0a58b1a2a01def862ea6f91bf273bfd2e28e1ef
Pierre <pgadea7@gmail.com>
2017-07-26 20:57:10 -0500

    added GET and POST

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 About:
 This is our first Evening-PT, Summer 2017 Web-Dev class project. It is a fullstack application that utilizes MongoDB, Node, ExpressJS, Mongoose, and VueJS.
+
+
+Asynchronous functions:  it will continue running while the other action is running
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const Superhero =  require('./models/Superhero')
+
 const app = express()
 let port = 3000
 
@@ -10,6 +12,39 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+// GET ALL Superheroes
+app.get('/api', (req,res) => {
+  Superhero.find((err, superheroes) => {
+    if(err){
+      res.json({message: err, data: null})
+    }
+    else{
+      console.log("Now get all")
+      res.json({message: `Successfully retrieved all heroes.`, data: superheroes})
+    }
+  })
+  console.log("No, now me")
+})
+
+// POST NEW HERO
+app.post('/api', (req, res) => {
+
+  // CREATE a new hero via Superhero constructor
+  let newHero = new Superhero()
+
+  newHero.name = req.body.name
+  newHero.superpower = req.body.superpower
+  newHero.image = req.body.image
+
+  newHero.save((err, newHero) => {
+    if(err){
+      res.json({message: err, data: null})
+    }
+    else{
+      res.json({message: `Successfully created new hero: ${newHero.name}`, data: newHero})
+    }
+  })
+})
 
 
 
