7a347b807c18ca6d320e57659a80f65c80c484dc
Pierre <pgadea7@gmail.com>
2017-07-27 22:30:33 -0500

    completed superheroes main page and create hero page

--- public/hero.js
+++ public/hero.js
@@ -1,10 +1,16 @@
 const title = "Superheroes Advanced"
+const heroFormTitle = "Create New Hero"
 
 const app = new Vue({
   el: '#app',
   data: {
     title: title,
     heroes: undefined,
+    heroFormTitle: heroFormTitle,
+    name: '',
+    superpower: '',
+    image: ''
+
   },
   created(){
     this.loadData()
@@ -17,6 +23,20 @@ const app = new Vue({
           url: '/api/superheroes',
           method: 'GET'
         }).done((response) => self.heroes = response.data)
+    },
+    createHero: function(){
+      let self = this
+      let newHero = {
+                      name: this.name,
+                      superpower: this.superpower,
+                      image: this.image
+                    }
+
+      $.ajax({
+        url: '/api/superheroes',
+        method: 'POST',
+        data: newHero
+      }).done((response) => console.log(response))
     }
   }
 })
