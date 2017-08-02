9e76f99f30774e7f936de320881d3980cb084b27
Pierre <pgadea7@gmail.com>
2017-08-01 23:02:08 -0500

    added Edit and Delete buttons

--- public/hero.js
+++ public/hero.js
@@ -9,27 +9,29 @@ const app = new Vue({
     heroFormTitle: heroFormTitle,
     name: '',
     superpower: '',
-    image: ''
-
+    image: '',
+    selectedHero: undefined,
+    heroIndex: undefined,
+    showEdit: false
   },
   created(){
     this.loadData()
   },
   methods: {
     loadData: function(){
-        let self = this
+        const self = this
 
         $.ajax({
           url: '/api/superheroes',
           method: 'GET'
         }).done((response) => self.heroes = response.data)
     },
-    createHero: function(){
-      let self = this
-      let newHero = {
-                      name: this.name,
-                      superpower: this.superpower,
-                      image: this.image
+    createHero(){
+      const self = this
+      const newHero = {
+                      name: self.name,
+                      superpower: self.superpower,
+                      image: self.image
                     }
 
       $.ajax({
@@ -37,6 +39,37 @@ const app = new Vue({
         method: 'POST',
         data: newHero
       }).done((response) => console.log(response))
+    },
+    deleteHero(hero_id){
+      const self = this
+
+      $.ajax({
+        url: `/api/superheroes/${hero_id}`,
+        method: 'DELETE',
+      }).done((response) => self.loadData())
+    },
+    toggleEdit(hero_id, index){
+      this.showEdit = !this.showEdit
+      this.selectedHero = (this.showEdit) ? hero_id : undefined
+      this.heroIndex = (this.showEdit) ? index : undefined
+    },
+    editHero(){
+      const self = this
+
+      const modifiedHero = {
+        name: (self.name.length > 0) ? self.name : null,
+        superpower: (self.superpower.length > 0) ? self.superpower : null,
+        image: (self.image.length > 0) ? self.image : null
+      }
+
+      $.ajax({
+        url: `/api/superheroes/${self.selectedHero}`,
+        method: 'PUT',
+        data: modifiedHero
+      }).done((response) => {
+        self.heroes[self.heroIndex] = response.data
+        self.showEdit = false
+      })
     }
   }
 })
